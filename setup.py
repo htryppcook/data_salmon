3dc332b7ba29c2bf7f26826a5033b425abfce9ef
Harrison <harrison@sounderintelligence.com>
2018-09-11 15:53:38 -0400

    adding setup files / requirements

---
+++ setup.py
@@ -0,0 +1,14 @@
+
+from setuptools import setup
+
+setup(
+  name='data_salmon',
+  version='0.0.1',
+  author='htryppcook',
+  setup_requires=[],
+  install_requires=[],
+  packages=[
+      'data_salmon'
+  ]
+)
+
