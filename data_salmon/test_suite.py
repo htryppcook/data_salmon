469e8b25a2ef1837f713caf8f2e5fc376bbe56a9
druid <druid@forest.grove>
2018-12-05 18:39:34 -0500

    clean up coverage section of nose call.

--- data_salmon/test_suite.py
+++ data_salmon/test_suite.py
@@ -7,14 +7,18 @@ class TestSuite:
     @classmethod
     def run(cls):
         file_path = path.join(path.dirname(path.abspath(__file__)), 'tests')
+        cover_packages = [
+            'data_salmon.data_salmon',
+            'data_salmon.cmd',
+            'data_salmon.dataset',
+            'data_salmon.dataset_evaluator',
+            'data_salmon.dataset_writer',
+            'data_salmon.errors',
+            'data_salmon.fields',
+            'data_salmon.input_file_loader',
+            'data_salmon.strategies'
+        ]
         nose.run(argv=[file_path, '--rednose', '--nologcapture',
-            '--with-coverage', '--cover-erase',
-            '--cover-package=data_salmon.data_salmon',
-            '--cover-package=data_salmon.cmd',
-            '--cover-package=data_salmon.dataset',
-            '--cover-package=data_salmon.dataset_evaluator',
-            '--cover-package=data_salmon.errors',
-            '--cover-package=data_salmon.fields',
-            '--cover-package=data_salmon.input_file_loader',
-            '--cover-package=data_salmon.strategies'
-        ])
+            '--with-coverage', '--cover-erase']
+            + ['--cover-package={}'.format(p) for p in cover_packages]
+        )
