277a5806e71cdf29d9e995ce12163a1a9a4269c9
druid <druid@forest.grove>
2018-12-05 19:08:16 -0500

    Give pass/fail when run from script, that + full coverage + missing lines using UnitTestDockerfile.

--- data_salmon/test_suite.py
+++ data_salmon/test_suite.py
@@ -6,19 +6,5 @@ from os import path
 class TestSuite:
     @classmethod
     def run(cls):
-        file_path = path.join(path.dirname(path.abspath(__file__)), 'tests')
-        cover_packages = [
-            'data_salmon.data_salmon',
-            'data_salmon.cmd',
-            'data_salmon.dataset',
-            'data_salmon.dataset_evaluator',
-            'data_salmon.dataset_writer',
-            'data_salmon.errors',
-            'data_salmon.fields',
-            'data_salmon.input_file_loader',
-            'data_salmon.strategies'
-        ]
-        nose.run(argv=[file_path, '--rednose', '--nologcapture',
-            '--with-coverage', '--cover-erase']
-            + ['--cover-package={}'.format(p) for p in cover_packages]
-        )
+        file_path = path.join('/', 'app', 'data_salmon')
+        nose.run(argv=[file_path, '--rednose', '--nologcapture'])
